359eed3b25a5378748935e05d0c662b85fb00d28
Liubomyr <liubomyrromankiv@gmail.com>
2017-07-10 16:18:46 +0300

    SearchBar component modified

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			term: ''
+		};
+		this.onInputChange = this.onInputChange.bind(this);
+	} 
+	onInputChange(e){
+		this.setState({term: e.target.value});
+		console.log(e.target.value);
+	}
 	render() {
 		return (
 			<form className="input-group">
-				<input />
+				<input 
+					value={this.state.term} 
+					placeholder="Get a five day forecast in your favourite cities."
+					className="form-control"
+					onChange={this.onInputChange}/>
 				<span className="input-group-btn">
 					<button type="submit" className="btn btn-secondary">Submit</button>
 				</span>
