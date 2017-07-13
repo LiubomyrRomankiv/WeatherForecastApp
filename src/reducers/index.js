c1a5f464edf5aa8a494dda46c4733a6b34af923d
Liubomyr <liubomyrromankiv@gmail.com>
2017-07-13 12:48:32 +0300

    WeatherList modified

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from 'redux';
 import WeatherReduser from './reduser_weather';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  // state: (state = {}) => state
+  weather: WeatherReduser
 });
 
 export default rootReducer;
